bfaac73ce1816eaad4c6765461404a3784e94682
Ray Ferric <63957587+rayferric@users.noreply.github.com>
2020-08-17 11:55:19 +0200

    Upload files

---
+++ src/test/java/com/rayferric/comet/MainTest.java
@@ -0,0 +1,12 @@
+package com.rayferric.comet;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void test() {
+        assertEquals(0, 1);
+    }
+}
